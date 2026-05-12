050b35d9a0e4424dfffa499a38a22f935facc4f9
Purbasa Maharana <purbasamaharana@gmail.com>
2026-05-12 12:55:21 +0530

    Update TextCleaner.java

--- src/main/java/com/ai_summary/backend/util/TextCleaner.java
+++ src/main/java/com/ai_summary/backend/util/TextCleaner.java
@@ -1,4 +1,12 @@
 package com.ai_summary.backend.util;
 
 public class TextCleaner {
+    public static String clean(String text) {
+        if (text == null) return "";
+        return text
+                .replaceAll("<[^>]+>", "")
+                .replaceAll("\\s+", " ")
+                .replaceAll("[^\\x20-\\x7E]", "")
+                .trim();
+    }
 }
