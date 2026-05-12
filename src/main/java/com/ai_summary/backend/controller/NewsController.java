64bb1d0c94de044ea410eb4d576be5950b06bd1f
Purbasa Maharana <purbasamaharana@gmail.com>
2026-05-12 13:55:22 +0530

    Update NewsController.java

--- src/main/java/com/ai_summary/backend/controller/NewsController.java
+++ src/main/java/com/ai_summary/backend/controller/NewsController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 
 @CrossOrigin(origins = {
-    "http://localhost:5173",
-    "https://ai-news-summary-frontend-model-pvso8j4fu-melonmusk20s-projects.vercel.app"
+        "http://localhost:5173",
+        "https://ai-news-summary-frontend-model.vercel.app"
 })
 @RestController
 public class NewsController {
